ede484c1de4bd85dec26ed1d424977efa709f85c
Saurabh Srivastava <sobu86@gmail.com>
2020-10-31 12:05:48 +0530

    First Commit with IUserActivity interface

---
+++ DotNetUtils.Win32/IUserActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DotNetUtils.Win32
+{
+    public interface IUserActivity
+    {
+        /// <summary>
+        /// This method fetches the time of last user interaction with the
+        /// system.
+        /// </summary>
+        /// <returns>
+        /// The time returned is in milliseconds since the system bootup.
+        /// </returns>
+        uint GetLastUserInputTime();
+
+        /// <summary>
+        /// Threshold for user inactivity in seconds when to invoke the
+        /// UserInactiveCallback
+        /// </summary>
+        uint UserInactivityThresholdSec { get; set; }
+
+        delegate void UserInactiveCallbackType();
+
+        /// <summary>
+        /// The Callback delegate invoked when user has been inactive for
+        /// the specified threshold.
+        /// </summary>
+        UserInactiveCallbackType UserInactiveCallback { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# DotNetUtils.Win32
+.NET Wrapper for Win32 Native APIs
+
+Native references taken from pinvoke.net
